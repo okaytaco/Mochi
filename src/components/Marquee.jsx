6fded331cbba486adc3cd374e034bc11cc3e80ca
Aniket <singhaniketk707@gmail.com>
2025-09-04 16:35:38 +0530

    marquee

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Marquee() {
+  return (
+    <div className="w-full h-[50vh] bg-[#210f04]">
+        <div className="text"></div>
+    </div>
+  )
+}
+
+export default Marquee
