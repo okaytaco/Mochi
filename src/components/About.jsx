cdbaa87363a15ada992daf8672552452fc3f838e
Aniket <singhaniketk707@gmail.com>
2025-09-06 19:41:10 +0530

    Updated About section (Added Image and texts)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className="w-full py-20 px-20 bg-[#A78C68] rounded-tl-3xl rounded-tr-3xl text-black">
+      <h1 className="text-[3.5vw] leading-[4vw] tracking-tight">Mochi is a strategic partner for fast-growing
+         tech buisnesses that need
+        to raise funds, sell products, explain complex ideas, and hire great
+        people.
+      </h1>
+      <div className="w-full flex gap-5 border-t-[1px] mt-20 pt-10">
+        <div className="w-1/2">
+        <h1 className="text-7xl">
+            Our Approach :  <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum error expedita ad laboriosam beatae?
+        </h1>
+        <button className="flex gap-10 items-center px-10 py-6 bg-zinc-900 mt-10 rounded-full text-white">READ MORE
+          <div className="w-3 h-3 bg-zinc-100 rounded-full"></div>
+        </button>
+        </div>
+        <img src="src/assets/about.jpeg" className="w-1/2 h-[70vh] rounded-3xl" />
+
+      </div>
+    </div>
   )
 }
 
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Marquee() {
   return (
     <div className="w-full py-10 rounded-tl-3xl rounded-tr-3xl bg-[#210f04]">
-        <div className="text border-t-1 border-b-1 border-[#7F562A] flex gap-70 overflow-hidden whitespace-nowrap">
+        <div className="text border-t-1 border-b-1 border-[#7F562A] flex gap-30 overflow-hidden whitespace-nowrap">
             <motion.h1 
             initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}}
              className="text-[#f8ebc5] text-[22vw] leading-none font-semibold pb-9">WE ARE MOCHI</motion.h1>
